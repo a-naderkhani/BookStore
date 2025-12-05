92ddfc55bea21069cda6fa43d414c086ea780ae4
a.h naderkhani <h_naderkhani_2010@yahoo.com>
2025-12-05 21:51:23 +0330

    creat Database

---
+++ BookStoreAdmin/Models/Entities/Book/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreAdmin.Models.Entities.Book;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Subject { get; set; }
+    public string BookSummery { get; set;}
+}
+
