105dd1b340e54cb6e0d4fdb54561e168fae3f984
a.h naderkhani <h_naderkhani_2010@yahoo.com>
2025-12-05 21:30:38 +0330

    Add project files.

---
+++ BookStoreAdmin/Controllers/AddBookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAdmin.Controllers
+{
+    public class AddBookController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Title,string Author,string Subject,string BookSummery)
+        {
+            return View(); 
+        }
+    }
+}
