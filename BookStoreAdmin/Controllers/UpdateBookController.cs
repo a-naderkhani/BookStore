e22ca91a3f68beb4a1ca10773daf706766451117
a.h naderkhani <h_naderkhani_2010@yahoo.com>
2025-12-11 12:15:47 +0330

    The project was completed

---
+++ BookStoreAdmin/Controllers/UpdateBookController.cs
@@ -0,0 +1,38 @@
+﻿using BookStoreAdmin.Models.BookStoreDbContext;
+using BookStoreAdmin.Models.Entities.Book;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAdmin.Controllers
+{
+    public class UpdateBookController : Controller
+    {
+        private readonly BookStoreDbContext _bookStoreDbContext;
+        public UpdateBookController(BookStoreDbContext bookStoreDbContext)
+        {
+            _bookStoreDbContext = bookStoreDbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int Id,string Title, string Author, string Subject, string BookSummery)
+        {
+            var book = _bookStoreDbContext.Books.FirstOrDefault(b => b.Id == Id);
+            if (Title != null) 
+            book.Title = Title;
+            if(Author != null)
+            book.Author = Author;
+            if(Subject != null) 
+            book.Subject = Subject;
+            if(BookSummery != null)
+            book.BookSummery = BookSummery;
+         
+            _bookStoreDbContext.Books.Update(book);
+            _bookStoreDbContext.SaveChanges();
+
+            return View();
+        }
+    }
+}
